46dd8290e026decc68a50b6a749e55f5fdd91de5
Olga <olgamarkova85@ya.ru>
2023-02-17 13:09:49 +0300

    Добавлены методы для создания массива из заданного количества элементов и для печати массива

--- Task/Program.cs
+++ Task/Program.cs
@@ -2,3 +2,28 @@
 // формирует массив из строк, длинна которых меньше либо равна 3 символа.
 // Первоначальный массив можно ввести с клавиатуры либо задать на старте
 
+Console.Write("Введите количество элементов массива: ");
+int elements = Convert.ToInt32(Console.ReadLine());
+
+
+string[] CreateArrayString(int elem)
+{
+    string[] array = new string[elements];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите набор символов для {i} элемента массива: ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write(array[i] + ", ");
+        else Console.Write(array[i]);
+    }
+    Console.WriteLine("]");
+}
